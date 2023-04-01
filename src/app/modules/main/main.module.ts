67873b672db16fd175f611f17f39bee571490d98
rohit77 <rohit.webmobinfo.ch>
2023-04-01 16:51:06 +0530

    api-compo-01Apr

---
+++ src/app/modules/main/main.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    SignupComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    SharedModule
+  ]
+})
+export class MainModule { }
---
+++ src/app/modules/main/services/main-api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from 'src/app/shared/services/http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainApiService {
+
+  constructor(private httpService: HttpService) { }
+
+  signup(payload = {}) {
+    return  this.httpService.post('/signup', payload);
+  }
+
+  getUsers(params = {}) {
+    return this.httpService.get('/get-users', params);
+  }
+}
