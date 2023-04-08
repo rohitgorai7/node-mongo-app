6968d59a5f08e8cb6ec4dd20513abb90b8524a30
rohit77 <rohit.webmobinfo.ch>
2023-04-08 22:22:15 +0530

    changes

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,28 +1,40 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { firstValueFrom } from 'rxjs';
+import { MainService } from 'src/app/modules/main/services/main.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private mainService: MainService) { }
 
   get(url: string, params: {}) {
-    return firstValueFrom(this.http.get(environment.apiUrl + url, params));
+    return firstValueFrom(this.http.get(environment.apiUrl + url, {...(params? {params: params}: {}), ...this.getHeaders()}));
   }
 
   post(url: string, payload: any) {
-    return firstValueFrom(this.http.post(environment.apiUrl + url, payload));
+    return firstValueFrom(this.http.post(environment.apiUrl + url, payload, {...this.getHeaders()}));
   }
 
   put(url: string, payload: any) {
-    return firstValueFrom(this.http.put(environment.apiUrl + url, payload));
+    return firstValueFrom(this.http.put(environment.apiUrl + url, payload, {...this.getHeaders()}));
   }
 
-  delete(url: string, payload: any) {
-    return firstValueFrom(this.http.delete(environment.apiUrl + url, payload));
+  delete(url: string, params: {}) {
+    return firstValueFrom(this.http.delete(environment.apiUrl + url, {...(params ? {params: params}: {}), ...this.getHeaders()}));
+  }
+
+  getHeaders() {
+    const user: any = this.mainService.getUserData();
+    return {
+      ...{
+        headers: new HttpHeaders({
+          ...(user ? {"x-access-token": user.token} : {})
+        })
+      }
+    }
   }
 }
