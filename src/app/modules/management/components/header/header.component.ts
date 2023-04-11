3cd87a277b8ab79220074c36b002e5af50a5b036
rohit77 <rohit.webmobinfo.ch>
2023-04-11 22:25:20 +0530

    changes

--- src/app/modules/management/components/header/header.component.ts
+++ src/app/modules/management/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MainService } from 'src/app/modules/main/services/main.service';
+import { WebApiService } from 'src/app/shared/services/web-api.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private webApiService: WebApiService, private ngxSpinnerService: NgxSpinnerService,private router: Router, private mainService: MainService) { }
 
   ngOnInit(): void {
+  }
+
+  async logout() {
+    try {
+      this.ngxSpinnerService.show();
+      const response = await this.webApiService.logout();
+      console.log(response);
+      this.mainService.clearSession();
+      this.router.navigate(['/']);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.ngxSpinnerService.hide();
+    }
   }
 
 }
